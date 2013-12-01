e19e56eab454780be8ef4460656bb7b4ab20bb11
jiu9x9uij <jiu9x9uij@gmail.com>
2013-12-01 15:00:38 -0500

    Fixed Black Box Defect #2.

--- src/edu/purdue/cs408/testify/InstructorFirstScreen.java
+++ src/edu/purdue/cs408/testify/InstructorFirstScreen.java
@@ -98,13 +98,13 @@ public class InstructorFirstScreen extends javax.swing.JFrame {
 
     private void editTestButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_editTestButtonActionPerformed
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        GraderTestify.main(new String[] {""});
+        InstructorTestify.main(new String[] {""});
         this.dispose();
     }//GEN-LAST:event_editTestButtonActionPerformed
 
     private void gradeTestButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_gradeTestButtonActionPerformed
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        InstructorTestify.main(new String[] {""});
+        GraderTestify.main(new String[] {""});
         this.dispose();
     }//GEN-LAST:event_gradeTestButtonActionPerformed
 
